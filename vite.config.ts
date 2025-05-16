8ba5c633745aadb9c1279f9bdcb03a7c3b84d1f4
BluOwn <teba.741@gmail.com>
2025-05-16 22:42:54 +0400

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: true,
+  build: {
+    // დავრწმუნდეთ, რომ სტატიკური ფაილები სწორად კოპირდება
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
   },
 });
