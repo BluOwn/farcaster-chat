d7bc2d37dec708d5c14bd8fc5c88b1c1e5d16d98
BluOwn <teba.741@gmail.com>
2025-05-17 11:00:55 +0400

    up

--- src/types/vite-config.d.ts
+++
@@ -1,9 +0,0 @@
-// vite.config.d.ts
-declare module 'vite' {
-  export function defineConfig(config: any): any;
-}
-
-declare module '@vitejs/plugin-react' {
-  const react: (options?: any) => any;
-  export default react;
-}
--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMeta {
-  readonly env: {
-    readonly VITE_CONTRACT_ADDRESS?: string;
-    readonly VITE_MULTISYNQ_API_KEY?: string;
-    readonly [key: string]: string | undefined;
-  };
-}
